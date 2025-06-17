e2a4862406fcc11dc67749476ae57a5243e4fb75
beyito <cobubenjamin898@gmail.com>
2025-06-17 17:25:05 -0400

    ..

--- client/src/Layouts/PerfilLayout.jsx
+++ client/src/Layouts/PerfilLayout.jsx
@@ -3,7 +3,9 @@ import { Outlet } from 'react-router'
 import { useAuth } from '@/context/AuthContext'
 import { CargaDeEspera } from '@/components/loading/CargaDeEspera'
 export const PerfilLayout = () => {
+
   const { isAuthenticated, user, editarUsuario } = useAuth()
+  console.log(user)
   return (
     <>
       {isAuthenticated ? <Outlet context={{ user, editarUsuario }} /> : <CargaDeEspera />}
