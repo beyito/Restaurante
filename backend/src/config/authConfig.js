6a154ec7ee9536c57ed3084cd89eba80bae6e24b
beyito <cobubenjamin898@gmail.com>
2025-06-17 18:41:38 -0400

    si1

--- backend/src/config/authConfig.js
+++ backend/src/config/authConfig.js
@@ -2,11 +2,9 @@ export const {
   PALABRA_SECRETA = 'clave_Secreta_para_el_restaurante_de_la_universidad',
   expiresIn = '1h',
   cookieOptions = {
-    httpOnly: true, // Más seguro que false (protege contra XSS)
-    secure: true, // Siempre true en producción (HTTPS obligatorio)
-    sameSite: 'strict', // Más restrictivo que 'lax' para seguridad
-    maxAge: 1000 * 60 * 60 * 24 * 7, // Ejemplo: 1 semana (ajusta según necesites)
-    path: '/', // Disponible en todas las rutas
-    signed: true // Recomendado para cookies importan
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None',
+    maxAge: 1000 * 60 * 60
   }
 } = process.env
